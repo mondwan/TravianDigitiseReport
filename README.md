861ee6245a511abe3c88d98c7058c2af49e2e102
Mond Wan <mondwan.1015@gmail.com>
2015-09-30 00:31:18 +0800

    Upload source code

---
+++ README.md
@@ -0,0 +1,6 @@
+# About
+
+This is a script runs on Tampermonkey which aims on digitise report info
+in Travian
+
+@author: Mond Wan <mondwan.1015@gmail.com>
---
+++ Travian digitise report.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Travian digitise report
+// @namespace    https://github.com/mondwan
+// @description  Digitalize resources directly on the page
+// @author       Mond Wan
+// @include      http://*.travian.*/berichte.php*
+// @grant        none
+// @run-at document-end
+// @version      1
+// ==/UserScript==
+
+(function () {
+    // Get all elements with report info
+    var bags = document.getElements('img.reportInfo');
+
+    // Digitise carry info in each bag
+    bags.forEach(function (bag) {
+        // Calculate load rate
+        var stat = bag.alt;
+        var tokens = stat.split('/');
+        var actualLoad = parseInt(tokens[0], 10);
+        var maxLoad = parseInt(tokens[1], 10);
+        var load = actualLoad / maxLoad * 100;
+
+        // An element carray load rate
+        var report = document.createElement('span');
+        load = load.toFixed(2);
+        report.innerText = load + '%';
+
+        // Render that element in DOM
+        var container = bag.parentElement;
+        container.appendChild(report);
+    });
+})();
